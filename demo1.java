ad03859c991ad64223eddd2447037cb79510158e
Nimesh Sharma <nimesh9528@gmail.com>
2021-09-30 23:58:45 +0530

    second commit

---
+++ demo1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class demo1
+{
+	public static void main(String[] v)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=nextInt();
+		int b= new Int();
+		System.out.println("sum is :"+a+b);
+		System.out.println("thank you");
+	}
+
+}
